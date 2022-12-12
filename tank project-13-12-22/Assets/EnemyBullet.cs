c99bde9d9c978adc3db720005227a393d880eedb
B00147824 <B00147824@mytudublin.ie>
2022-12-12 19:33:47 +0000

    last-13-12-22

---
+++ tank project-13-12-22/Assets/EnemyBullet.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public Rigidbody rb;
+    
+    void Start()
+    {
+        rb.GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * 12;
+    }
+
+   
+
+}
